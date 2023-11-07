498bb05f4e0026e0db20f22586bac244b8ebd312
FranciscoGT <143522959+FranciscoGT@users.noreply.github.com>
2023-11-07 00:08:12 -0300

    Add files via upload

---
+++ Mian/src/mian/Mian.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mian;
+
+import Views.View;
+
+/**
+ *
+ * @author frano
+ */
+public class Mian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new View().setVisible(true);
+    }
+    
+}
